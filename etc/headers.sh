6895ef2f32c937851417ffe7cfdd1e70ee8f06bf
Tim Menzies <tim.menzies@gmail.com>
2020-05-13 21:27:34 -0400

    pushing

--- etc/headers.sh
+++ etc/headers.sh
@@ -8,9 +8,7 @@ cat <<EOF> /tmp/heads$$
 <a class=sehero name=top> 
 <p><img align=right width=300
 src="https://cdn.pixabay.com/photo/2019/08/01/21/40/spiderman-4378357_1280.png"></p>
-<h1> SE for super heroes: an AI approach </h1> <p> <a
-href="$r/README.md#top">home</a> | <a
-href="$u">code</a> | <a
+<h1><a href="$f/README.md#top">SE for super heroes: an AI approach</a></h1> <p> <a
 href="$r/LICENSE">license</a> | <a
 href="$r/INSTALL.md#top">install</a> | <a
 href="$r/CODE_OF_CONDUCT.md#top">contribute</a> | <a
